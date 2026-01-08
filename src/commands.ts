03560ecf599888a45bc42581c252628bae94dce9
AadityaNair <AadityaNair@users.noreply.github.com>
2026-01-08 21:06:52 +0000

    Refactor and fix some stuff

--- src/commands.ts
+++ src/commands.ts
@@ -8,18 +8,25 @@
 export function handleQuery(q: string): string {
 	// For now, we'll just redirect to a Google search.
 	// This can be expanded with more complex logic.
-	if (q.startsWith('!')) {
-		// Example of a simple "bang" command
-		const command = q.substring(1).split(' ')[0];
-		const rest = q.substring(command.length + 2);
-		switch (command) {
-			case 'gh':
-				return `https://github.com/search?q=${encodeURIComponent(rest)}`;
-			case 'npm':
-				return `https://www.npmjs.com/search?q=${encodeURIComponent(rest)}`;
-			default:
-				return `https://www.google.com/search?q=${encodeURIComponent(q)}`;
-		}
+	const command = q.split(' ')[0];
+	const args = q.substring(command.length + 2)
+	switch (command) {
+		case 'gh':
+			return `https://github.com/search?q=${encodeURIComponent(rest)}`;
+		case 'npm':
+			return `https://www.npmjs.com/search?q=${encodeURIComponent(rest)}`;
 	}
-	return `https://www.google.com/search?q=${encodeURIComponent(q)}`;
+	return googleSearch(q);
+}
+
+export function googleSearch(q: string): string {
+	var search_string: string = q;
+	if q.startsWith('g ') {
+		search_string = q.split(' ')[1]
+	}
+	return `https://www.google.com/search?q=${encodeURIComponent(search_string)}`;
+}
+
+export function githubRepoOrSearch(q: string): string {
+
 }
